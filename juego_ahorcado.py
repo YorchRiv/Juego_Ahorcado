a55cb9a43d260a9f0e8efa5e13fb83579513d76b
Jorge Mejicanos <jorgemejicanos145@gmail.com>
2022-04-15 10:07:18 -0600

    Sistema de elecion de letras ya funciona

--- juego_ahorcado.py
+++ juego_ahorcado.py
@@ -15,6 +15,7 @@ def run():
     palabras = []
     letrax = []
     letra = ""
+    imp_pal = ""
     
 
     with open("./data.txt", "r", encoding="utf-8") as f:
@@ -26,23 +27,22 @@ def run():
     pal = str([palabras[i] for i in range(0, 171) if i == num_ale]) #Traer palabra aleatoria a variable pal
     str_pal = "".join(pal) #Convertir a string la variable pal
 
+    for i in range(0, len_string - 1):
+        letrax.append("_ ")
 
     while ganaste == False:
         print("Adivina la palabra!\n")
 
         for i in range(0, len_string - 1):
             if letra == str_pal[i + 2:i + 3:]:
-                print(str_pal[i + 2:i + 3:], end=" ")
-                letrax.append(str_pal[i + 2:i + 3:])
-
-            elif letrax[i] != "":
-                print(str_pal[i + 2:i + 3:], end=" ")
-                
-            else:                
-                print("_", end=" ")
-                letrax.append("")
+                # print(str_pal[i + 2:i + 3:], end=" ")
+                letrax[i] = str_pal[i + 2:i + 3:] + " "
+            # else:                
+                # print("_", end=" ")
 
         print(str_pal[2:len_string + 1:])
+        str_letrax = "".join(letrax) #Convertir letrax en string
+        print(str_letrax)
 
         letra = input("Ingrese una Letra: ")
 
